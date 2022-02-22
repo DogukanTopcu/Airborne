1383576bebf11a81fda6ba1f0a17d52c0293082b
DogukanTopcu <dogukan_topcu3575@icloud.com>
2022-02-22 15:41:06 +0300

    footer edited

--- src/Footer/footer.jsx
+++ src/Footer/footer.jsx
@@ -5,21 +5,26 @@ import "./footer.css";
 export default function Footer() {
     return (
         <footer>
-            <div id="footer-top">İLETİŞİM BİLGİLERİ</div>
-            <div id="adres">
-                <p>Adres:</p><p>Gülbahçe, İzmir Yüksek Teknoloji Enstitüsü, 35430 Urla/İzmir</p>
-            </div>
-            <div id="tel">
-                <p>Telefon Numarası:</p><p>+90 507 536 91 89</p>
-            </div>
-            <div id="social-media">
-                    <a href="https://www.instagram.com/airbornerocket/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
-                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                    <a href="https://www.linkedin.com/company/airborneroket" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
-            </div>
-            <div id='footer-bottom'>
-                <div id="copyright">© 2022 Airborne Rocket</div>
-                <div className='language'><a href='#'>English</a></div>
+            <div className="container" id='footer-text'>
+                <div id="footer-top">İLETİŞİM BİLGİLERİ</div>
+                <div id="adres">
+                    <p>Adres:</p><p>Gülbahçe, İzmir Yüksek Teknoloji Enstitüsü, 35430 Urla/İzmir</p>
+                </div>
+                <div id="tel">
+                    <p>Telefon Numarası:</p><p>+90 507 536 91 89</p>
+                </div>
+                <div id="social-media">
+                        <a href="https://www.instagram.com/airbornerocket/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="https://www.linkedin.com/company/airborneroket" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                </div>
+                <br />
+                <hr />
+                <br />
+                <div id='footer-bottom'>
+                    <div id="copyright">© 2022 Airborne Rocket</div>
+                    <div className='language'><a href='#'>English</a></div>
+                </div>
             </div>
         </footer>
     );
