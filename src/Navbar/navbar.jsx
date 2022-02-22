658c684f3cf5d250bd68ef88a76fe9540dbd3099
Adem Kılıç <ademk014689@gmail.com>
2022-02-22 22:07:22 +0300

    Just a few changes and a temporary dropdown.

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./navbar.css";
 //import "../bootstrap.css";
 
@@ -12,29 +12,48 @@ export default function Navbar(){
 }
 
 function Items(){
+
+  var dropdownContent=document.getElementsByClassName("lang-dropdown-content");
+  var signal="off"
+
+  function name() {
+    switch (signal){
+
+      case "off":
+        for (var i=0; i< dropdownContent.length; i++){
+          dropdownContent[i].style.display= "block";
+        }
+        signal= "on";
+        break;
+
+      case "on":
+        for (var i=0; i< dropdownContent.length; i++){
+          dropdownContent[i].style.display= "none";
+        }
+        signal= "off";
+        break;
+    }
+
+  }
+  
   return(
     <ul>
       <li className='items'><a href="#">DESTEK OL</a></li>
       <li className='items'><a href="#">İLETİŞİME GEÇ</a></li>
       <li className='items'><a href="#">GALERİ</a></li>
       <li className='items'><a href="#">HAKKIMIZDA</a></li>
-      <li className='items'><a href="#">HAKKIMIZDA</a></li>
-      <button id="lang" className="lang-dropdown">
+      <button onClick={()=>{name()}} id="lang" className="lang-dropdown">
         <li className='items'>
         <span className="span">TUR</span>
         <i className="fa fa-caret-down"></i>
         </li>
       </button>
         <div className="lang-dropdown-content">
-          <a href="#">Eng</a>
+          <a href="#">ENG</a>
         </div>
 
     </ul>
   );
 }
 
-// var dropdown= document.getElementsById("lang");
 
-// dropdown.onclick= function(){
-//   alert("merhab");
-// }
