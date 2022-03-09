bf111edfb4243f584485de455ecf84c06f6eb181
Adem Kılıç <ademk014689@gmail.com>
2022-03-09 18:29:03 +0300

    I have turned it to a bilungual web page.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-
+import React from "react";
 import Navbar from "./Navbar/navbar";
 import Footer from "./Footer/footer";
 import Body from "./Content/body"
 
-export default function App() {
-  return (
-    <div className="App">
-      <Container/>
-      <Footer/>
-    </div>
-  );
+class App extends React.Component {
+
+  state= {
+    
+    language1:"TUR",
+
+    langs: [
+      {
+        donate:"DONATE",
+        contact:"CONTACT",
+        gallery:"GALLERY",
+        about:"ABOUT US",
+        footerTitle:"CONTACT",
+        address:"Address:",
+        phone:"Phone Number:",
+        lang:"ENG"
+      },
+      {
+        donate:"DESTEK OL",
+        contact:"İLETİŞİME GEÇ",
+        gallery:"GALERİ",
+        about:"HAKKIMIZDA",
+        footerTitle:"İLETİŞİM BİLGİLERİ",
+        address:"Adres:",
+        phone: "Telefon Numarası:",
+        lang:"TUR"
+      }
+    ]
+
+  }
+  
+
+  setLang = (language) => {
+    this.setState({language1: language});
+  }
+
+  render(){
+
+    var langFiltered = this.state.langs.filter(
+      (langItem) => {
+          return langItem.lang === this.state.language1;
+      }
+    );
+
+    return (
+      <div className="App">
+        <dir className="container">
+          <Navbar
+            setLangProp={this.setLang}
+            langFilteredProp = {langFiltered}
+          />
+          <Body/>
+      </dir>
+        <Footer 
+          langFilteredProp={langFiltered}
+        />
+      </div>
+    );
+  };
 }
 
-function Container() {
-  return (
-    <dir className="container">
-      <Navbar/>
-      <Body/>
-    </dir>
-    );
-}
+export default App;
--- src/Footer/footer.jsx
+++ src/Footer/footer.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import "./footer.css";
 //import "../bootstrap.css";
 
-export default function Footer() {
+export default function Footer(props) {
     return (
         <footer>
             <div className="container" id='footer-text'>
-                <div id="footer-top">İLETİŞİM BİLGİLERİ</div>
+                <div id="footer-top">{props.langFilteredProp[0].footerTitle}</div>
                 <div id="adres">
-                    <p>Adres:</p><p>Gülbahçe, İzmir Yüksek Teknoloji Enstitüsü, 35430 Urla/İzmir</p>
+                    <p>{props.langFilteredProp[0].address}</p><p>Gülbahçe, İzmir Yüksek Teknoloji Enstitüsü, 35430 Urla/İzmir</p>
                 </div>
                 <div id="tel">
-                    <p>Telefon Numarası:</p><p>+90 507 536 91 89</p>
+                    <p>{props.langFilteredProp[0].phone}</p><p>+90 507 536 91 89</p>
                 </div>
                 <div id="social-media">
                         <a href="https://www.instagram.com/airbornerocket/" target="_blank"><i className="fa-brands fa-instagram"></i></a>
