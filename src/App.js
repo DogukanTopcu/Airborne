69c57d0174ab54380ad2a597da2a156fe804a8e2
Adem Kılıç <ademk014689@gmail.com>
2022-05-04 00:14:22 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -9,15 +9,11 @@ import {
 import Layout from "./Components/Layout/Layout";
 import Contact from "./Components/Contact/Contact";
 import Home from "./Home";
-<<<<<<< HEAD
 import Support from "./Components/Support/Support"
-=======
 import About from "./Components/About/About";
->>>>>>> 9b5bab711844eed2452c4b0e3b6ebf80de513265
 
 const App = () => {
 
-<<<<<<< HEAD
   return (
     <div>
       <Routes>
@@ -25,25 +21,12 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="contact" element={<Contact />} />
           <Route path="support" element={<Support/>}/>
+          <Route path="about" element={<About /> }/>
+          <Route path="contact" element={<Contact />}/>
         </Route>
       </Routes>
     </div>
   );
-=======
-  render(){
-    return(
-      <div>
-        <Routes>
-          <Route path="/" element={<Layout/>}>
-            <Route index element={<Home/>} />
-            <Route path="about" element={<About /> }/>
-            <Route path="contact" element={<Contact />}/>
-          </Route>
-        </Routes>
-      </div>
-    );
-  }
->>>>>>> 9b5bab711844eed2452c4b0e3b6ebf80de513265
 }
 
 export default App;
